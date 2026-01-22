c703b46ba7679ae1c94ca53bf9bc31d5b55507f4
YaReynoso <greatgraystar@icloud.com>
2026-01-21 16:38:42 -0800

    Bubbles moved and spawn randomly from the bottom

---
+++ Code/Main/Main.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name Main
+
+const BUBBLE = preload("res://Bubble/Bubble.tscn")
+
+func _ready() -> void:
+	spawn_bubble()
+
+func spawn_bubble():
+	var bubble: = BUBBLE.instantiate()
+	bubble.position = Vector2(randi_range(0, get_viewport().size.x), get_viewport().size.y)
+	add_child(bubble)
+	
